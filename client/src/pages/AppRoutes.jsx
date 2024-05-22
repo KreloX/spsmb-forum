2098ed0e53d1347c1f83e15942744df3dc408cc7
CornChasm <kornel.kazmierczak@student.spsmb.cz>
2024-05-22 15:07:12 +0200

    add profile page

--- client/src/pages/AppRoutes.jsx
+++ client/src/pages/AppRoutes.jsx
@@ -1,12 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './Home/Home'
 import Auth from './Auth/Auth'
-
+import Profile from './Profile/Profile'
 export default () => (
     <BrowserRouter>
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/auth" element={<Auth />} />
+            <Route path="/profile" element={<Profile />} />
         </Routes>
     </BrowserRouter>
 )
