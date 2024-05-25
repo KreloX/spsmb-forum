3c673c6c034dbbb58f301e594e1cdad0eeca0f6e
Martin <martinhercik16@gmail.com>
2024-05-25 09:28:17 +0200

    forums renamed to threads; update method improved

--- server/app.js
+++ server/app.js
@@ -12,7 +12,7 @@ mongoose
 
 const indexRouter = require('./routes/index')
 const usersRouter = require('./routes/users')
-const forumsRouter = require('./routes/forums')
+const threadsRouter = require('./routes/threads')
 
 const app = express()
 
@@ -29,7 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
-app.use('/forums', forumsRouter) 
+app.use('/threads', threadsRouter) 
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
